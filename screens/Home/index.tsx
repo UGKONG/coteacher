4d979eea61f3e7918dc221e90b6dc19ea6128836
UGKONG <jsw9330@icloud.com>
2023-02-20 12:03:43 +0900

    2023-02-20

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import YoutubePlayer from 'react-native-youtube-iframe';
+
 import {Text} from 'react-native';
 import Container from '../../layouts/Container';
 
@@ -7,6 +9,8 @@ export default function HomeScreen() {
   return (
     <Container.Scroll>
       <Text>HOME</Text>
+      <YoutubePlayer height={300} play={false} videoId={'iee2TATGMyI'} />
+      <Text>dd</Text>
     </Container.Scroll>
   );
 }
